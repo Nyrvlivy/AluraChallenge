fce5d10a64858321b8743aa0996f34191768896c
Nyrvlivy <nyrvlivy@gmail.com>
2024-03-27 04:24:18 -0300

    feat(service): implement allUsers method in UserService for fetching users

---
+++ src/main/java/br/com/alura/challenge/business/services/UserService.java
@@ -0,0 +1,26 @@
+package br.com.alura.challenge.business.services;
+
+import br.com.alura.challenge.infrastructure.entities.UserEntity;
+import br.com.alura.challenge.infrastructure.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<UserEntity> allUsers() {
+        List<UserEntity> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+
+}
