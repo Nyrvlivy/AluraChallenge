ca37458c07e7a448cc51683aa25d8743893b1af9
Nyrvlivy <nyrvlivy@gmail.com>
2024-03-27 04:24:07 -0300

    feat(users): create restricted endpoints in UserController for user retrieval

---
+++ src/main/java/br/com/alura/challenge/api/v1/controller/UserController.java
@@ -0,0 +1,41 @@
+package br.com.alura.challenge.api.v1.controller;
+
+
+import br.com.alura.challenge.business.services.UserService;
+import br.com.alura.challenge.infrastructure.entities.UserEntity;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@Tag(name = "Users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserEntity> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        UserEntity currentUser = (UserEntity) authentication.getPrincipal();
+
+        return ResponseEntity.ok(currentUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserEntity>> allUsers() {
+        List<UserEntity> users = userService.allUsers();
+
+        return ResponseEntity.ok(users);
+    }
+}
