16997100b5f41c49629d5d46bef4cfe005438598
Nyrvlivy <nyrvlivy@gmail.com>
2024-03-27 04:30:05 -0300

    feat(auth): update RegisterUserDTO

--- src/main/java/br/com/alura/challenge/api/v1/dto/RegisterUserDTO.java
+++ src/main/java/br/com/alura/challenge/api/v1/dto/RegisterUserDTO.java
@@ -1,14 +1,20 @@
 package br.com.alura.challenge.api.v1.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RegisterUserDTO {
+
+    @JsonProperty(value = "name")
     private String name;
+    @JsonProperty(value = "username")
     private String username;
+    @JsonProperty(value = "email")
     private String email;
+    @JsonProperty(value = "password")
     private String password;
 
 }
