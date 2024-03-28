460f3d9df64a3bfbbbc3bd408b672f5ab2b20f0c
Nyrvlivy <nyrvlivy@gmail.com>
2024-03-28 03:08:14 -0300

    docs(swagger): update Rating Controller documentation

--- src/main/java/br/com/alura/challenge/api/v1/controller/RatingController.java
+++ src/main/java/br/com/alura/challenge/api/v1/controller/RatingController.java
@@ -3,24 +3,35 @@ package br.com.alura.challenge.api.v1.controller;
 import br.com.alura.challenge.api.v1.dto.RatingDTO;
 import br.com.alura.challenge.api.v1.dto.response.RatingResponseDTO;
 import br.com.alura.challenge.business.services.RatingService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ratings")
+@RequiredArgsConstructor
+@Tag(name = "Ratings", description = "Operations related to course ratings.")
 public class RatingController {
 
     private final RatingService ratingService;
 
-    @Autowired
-    public RatingController(RatingService ratingService) {
-        this.ratingService = ratingService;
-    }
-
+    @Operation(summary = "Submit a Course Rating",
+            description = "Submits a rating for a course, including a numerical score and an optional comment. User privileges required.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rating submitted successfully",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RatingResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid data provided"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized - Requires authentication"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @CrossOrigin
     @PostMapping
     public ResponseEntity<RatingResponseDTO> rateCourse(@RequestBody RatingDTO ratingDTO) {
         RatingResponseDTO createdRating = ratingService.rateCourse(ratingDTO);
