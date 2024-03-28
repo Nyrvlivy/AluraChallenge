52e873d41f49b7a216e79a540a589c328a30f29f
Nyrvlivy <nyrvlivy@gmail.com>
2024-03-28 03:27:24 -0300

    docs(swagger): add Status Controller documentation

--- src/main/java/br/com/alura/challenge/api/v1/controller/StatusController.java
+++ src/main/java/br/com/alura/challenge/api/v1/controller/StatusController.java
@@ -1,5 +1,7 @@
 package br.com.alura.challenge.api.v1.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +10,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Tag(name = "Status", description = "Operations related to application status.")
 public class StatusController {
 
+    @Operation(summary = "Welcome", description = "Displays a welcome message.")
     @GetMapping("/")
     public String welcome() {
         return "Welcome to Alura Challenge!";
     }
 
+    @Operation(summary = "Application Status", description = "Provides a quick health check of the application.")
     @GetMapping("/status")
     public Map<String, Object> getAppStatus() {
         Map<String, Object> status = new HashMap<>();
